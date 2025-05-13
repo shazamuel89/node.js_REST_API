bb368ee91a8205b7d5c1dabb2cc8e83a2beb8ef8
Samuel Heinrich <shazamuel89@gmail.com>
2025-05-13 15:12:26 -0500

    Changed corsOptions.js file to be out of dev mode

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,15 +1,11 @@
 // Cross Origin Resource Sharing
-// REMOVE THE LAST 2 IN WHITELIST AND CHANGE YOURSITE TO MY WEBSITE URL BEFORE DEPLOYING
 const whitelist = [
-    'https://www.yoursite.com',
-    'http://127.0.0.1:5500',
-    'http://localhost:3500'
+    'https://bead-shine-celestite.glitch.me'
 ];
 
 const corsOptions = {
     origin: (origin, callback) => {
-        // REMOVE THE || !origin BEFORE DEPLOYING
-        if (whitelist.indexOf(origin) != -1 || !origin) {
+        if (whitelist.indexOf(origin) != -1) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'));
