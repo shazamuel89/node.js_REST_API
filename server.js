d5b21d8c127cb508f287456b3fa8986d8ce9488d
Samuel Heinrich <shazamuel89@gmail.com>
2025-05-15 18:11:38 -0500

    Changed corsOptions.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ connectDB();
 app.use(logger);
 
 // Cross Origin Resource Sharing
-app.use(cors(corsOptions));
+app.use(cors());
 
 // Built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
